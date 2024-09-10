61c86eb1fd081593a793e6c7ebcb8178fe759658
P.Vasavi <118538149+pvasaviyadav@users.noreply.github.com>
2024-09-10 10:54:35 +0530

    Create ConfigClass.java

---
+++ src/main/java/com/jsp/hotelmanagement/ConfigClass/ConfigClass.java
@@ -0,0 +1,26 @@
+package com.jsp.hotelmanagement.ConfigClass;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigClass {
+  
+	@Bean
+	public EntityManagerFactory getEmf() {
+		return Persistence.createEntityManagerFactory("dev");
+	}
+	@Bean
+	public EntityManager getEm() {
+		return getEmf().createEntityManager();
+	}
+	@Bean
+	public EntityTransaction getEt() {
+		return getEm().getTransaction();
+	}
+}
